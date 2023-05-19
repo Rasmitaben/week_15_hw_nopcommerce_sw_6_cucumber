d6872e5afdf8f6afaf2959334b812dab92fa076b
Rasmitaben <126806045+Rasmitaben@users.noreply.github.com>
2023-05-19 18:40:20 +0100

    Update RegressionTestRunner.java

--- src/test/java/com/nopcommerce/demo/runner/RegressionTestRunner.java
+++ src/test/java/com/nopcommerce/demo/runner/RegressionTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/resources/featurefile",
         glue = "com/nopcommerce/demo",
-        tags = "@sanity",
+        tags = "@regression",
         plugin = {"pretty", "html:target/cucumber-report/cucumber.html",
                 "com.cucumber.listener.ExtentCucumberFormatter:target/Extent_Reports/report.html",
                 "json:target/RunCuke/cucumber.json"}
